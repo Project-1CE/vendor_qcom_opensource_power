3b286a10f2273542ad3efef737c3dbe6c298e81b
Steve Kondik <steve@cyngn.com>
2015-10-19 14:43:39 -0700

    power: Add support for tap-to-wake feature control
    Squashed into this commit:
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sat Dec 7 23:51:29 2019 +0100
    
        power: Follow symlinks when writing to dt2w toggle path
    
        * Some devices tend to symlink touchpanel related toggles
          to common path due to having to deal with different panels.
    
        Change-Id: I429e4c5acfbcd3ff26a019f919fbe880692160bd
    
    [ArianK16a]: Adapt to AIDL HAL
    
    Change-Id: I545902b29f4828c127bc32def6e30b67ce4a3aa7

--- Power.cpp
+++ Power.cpp
@@ -31,6 +31,7 @@
 
 #include "Power.h"
 
+#include <android-base/file.h>
 #include <android-base/logging.h>
 
 #include <aidl/android/hardware/power/BnPower.h>
@@ -60,7 +61,13 @@ void setInteractive(bool interactive) {
 ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
     LOG(INFO) << "Power setMode: " << static_cast<int32_t>(type) << " to: " << enabled;
     switch(type){
+#ifdef TAP_TO_WAKE_NODE
         case Mode::DOUBLE_TAP_TO_WAKE:
+            ::android::base::WriteStringToFile(enabled ? "1" : "0", TAP_TO_WAKE_NODE, true);
+            break;
+#else
+        case Mode::DOUBLE_TAP_TO_WAKE:
+#endif
         case Mode::LOW_POWER:
         case Mode::LAUNCH:
         case Mode::DEVICE_IDLE:
@@ -102,6 +109,9 @@ ndk::ScopedAStatus Power::isModeSupported(Mode type, bool* _aidl_return) {
                 *_aidl_return = false;
             }
             break;
+#ifdef TAP_TO_WAKE_NODE
+        case Mode::DOUBLE_TAP_TO_WAKE:
+#endif
         case Mode::INTERACTIVE:
         case Mode::SUSTAINED_PERFORMANCE:
         case Mode::FIXED_PERFORMANCE:
